95f9fcf43358d7f8dd9df0edd942e34e7b7dd570
ChuSeongJun <j949854@gmail.com>
2025-06-10 00:07:37 +0900

    Sytle: 공통 색상 변수 코드 생성 -추후 ailwindCSS IntelliSense 확장 프로그램으로 워닝 제거

--- fronted/src/app/page.tsx
+++ fronted/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <>
-            <h1 className="text-3xl font-bold underline">Hello World</h1>
+            <h1 className="bg-blue-500 font-bold underline">Hello World</h1>
         </>
     );
 }
